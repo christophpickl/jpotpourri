774e343ca9f8f977ef65ab9f50a7b5e8491751d9
christoph_pickl <christoph_pickl@ad6cabb3-f050-0410-bdcd-8a5510bc7fc2>
2008-08-13 13:55:29 +0000

    got NullPointerEx on some system

--- jpotpourri/trunk/src/main/java/net/sourceforge/jpotpourri/tools/PtUserSniffer.java
+++ jpotpourri/trunk/src/main/java/net/sourceforge/jpotpourri/tools/PtUserSniffer.java
@@ -14,28 +14,6 @@ public final class PtUserSniffer {
     /**  */
     private static PtOperatingSystem os;
 
-    static { // TODO use lazy initializing instead
-        final String osname = System.getProperty("os.name");
-
-        if (osname.contains("Mac")) {
-            PtUserSniffer.os = PtOperatingSystem.MAC;
-        } else if (osname.contains("Windows")) {
-            PtUserSniffer.os = PtOperatingSystem.WIN;
-        } else if (osname.contains("Linux")) {
-            PtUserSniffer.os = PtOperatingSystem.LINUX;
-        } else if (osname.contains("Unix") || osname.contains("UNIX")  || osname.contains("HP-UX")
-                || osname.contains("AIX") || osname.contains("BSD") || osname.contains("Irix")) {
-            PtUserSniffer.os = PtOperatingSystem.UNIX;
-        } else if (osname.contains("SunOS") || osname.contains("Solaris")) {
-            PtUserSniffer.os = PtOperatingSystem.SOLARIS;
-        } else if (osname.contains("OS/2")) {
-            PtUserSniffer.os = PtOperatingSystem.OS2;
-        } else {
-            PtUserSniffer.LOG.warn("could not determine operating system by name [" + osname + "]");
-            PtUserSniffer.os = PtOperatingSystem.UNKOWN;
-        }
-        LOG.debug("seems as user is running '" + PtUserSniffer.os + "'.");
-    }
 
     
     
@@ -48,7 +26,31 @@ public final class PtUserSniffer {
      * @return
      */
     public static PtOperatingSystem getOS() {
-        return PtUserSniffer.os;
+        if(PtUserSniffer.os == null) {
+            final String osname = System.getProperty("os.name");
+
+            if (osname.contains("Mac")) {
+                PtUserSniffer.os = PtOperatingSystem.MAC;
+            } else if (osname.contains("Windows")) {
+                PtUserSniffer.os = PtOperatingSystem.WIN;
+            } else if (osname.contains("Linux")) {
+                PtUserSniffer.os = PtOperatingSystem.LINUX;
+            } else if (osname.contains("Unix") || osname.contains("UNIX")  || osname.contains("HP-UX")
+                    || osname.contains("AIX") || osname.contains("BSD") || osname.contains("Irix")) {
+                PtUserSniffer.os = PtOperatingSystem.UNIX;
+            } else if (osname.contains("SunOS") || osname.contains("Solaris")) {
+                PtUserSniffer.os = PtOperatingSystem.SOLARIS;
+            } else if (osname.contains("OS/2")) {
+                PtUserSniffer.os = PtOperatingSystem.OS2;
+            } else {
+                PtUserSniffer.LOG.warn("could not determine operating system by name [" + osname + "]");
+                PtUserSniffer.os = PtOperatingSystem.UNKOWN;
+            }
+            
+            assert(PtUserSniffer.os != null);
+            LOG.debug("Seems as user is running '" + PtUserSniffer.os + "'.");
+        }
+    	return PtUserSniffer.os;
     }
 
     /**
@@ -57,31 +59,31 @@ public final class PtUserSniffer {
      * @return
      */
     public static boolean isOperatingSystem(final PtOperatingSystem isOs) {
-        return PtUserSniffer.os.equals(isOs);
+        return PtUserSniffer.getOS().equals(isOs);
     }
 
     public static boolean isWindows() {
-        return PtUserSniffer.os.equals(PtOperatingSystem.WIN);
+        return PtUserSniffer.getOS().equals(PtOperatingSystem.WIN);
     }
 
     public static boolean isMacOSX() {
-        return PtUserSniffer.os.equals(PtOperatingSystem.MAC);
+        return PtUserSniffer.getOS().equals(PtOperatingSystem.MAC);
     }
 
     public static boolean isLinux() {
-        return PtUserSniffer.os.equals(PtOperatingSystem.LINUX);
+        return PtUserSniffer.getOS().equals(PtOperatingSystem.LINUX);
     }
 
     public static boolean isUnix() {
-        return PtUserSniffer.os.equals(PtOperatingSystem.UNIX);
+        return PtUserSniffer.getOS().equals(PtOperatingSystem.UNIX);
     }
 
     public static boolean isOS2() {
-        return PtUserSniffer.os.equals(PtOperatingSystem.OS2);
+        return PtUserSniffer.getOS().equals(PtOperatingSystem.OS2);
     }
 
     public static boolean isUnkown() {
-        return PtUserSniffer.os.equals(PtOperatingSystem.UNKOWN);
+        return PtUserSniffer.getOS().equals(PtOperatingSystem.UNKOWN);
     }
 
 }
