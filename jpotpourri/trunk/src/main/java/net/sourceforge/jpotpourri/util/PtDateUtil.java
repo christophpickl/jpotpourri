26dbd11daf275a42f515b8082341a15199cd0062
christoph_pickl <christoph_pickl@ad6cabb3-f050-0410-bdcd-8a5510bc7fc2>
2008-08-20 08:29:20 +0000

    introducing timestamp component

--- jpotpourri/trunk/src/main/java/net/sourceforge/jpotpourri/util/PtDateUtil.java
+++ jpotpourri/trunk/src/main/java/net/sourceforge/jpotpourri/util/PtDateUtil.java
@@ -1,5 +1,6 @@
 package net.sourceforge.jpotpourri.util;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
@@ -14,9 +15,13 @@ import java.util.List;
  * 
  * @author christoph_pickl@users.sourceforge.net
  */
-public class PtDateUtil {
+public final class PtDateUtil {
 
-    protected PtDateUtil() {
+	public static final String TIMESTAMP_FORMAT = "yyyy-MM-dd HH:mm:ss.SSSSSS";
+	
+	private static final DateFormat TIMESTAMP_FORMATTER = new SimpleDateFormat(TIMESTAMP_FORMAT);
+	
+    private PtDateUtil() {
         // no instantiation
     }
     
@@ -74,6 +79,129 @@ public class PtDateUtil {
     public static int compareWithoutMilliSeconds(final Date d1, final Date d2) {
         return getDateWithoutMilliSeconds(d1).compareTo(getDateWithoutMilliSeconds(d2));
     }
+
+    private static boolean validateTimestampNumber(
+    		final String string,
+    		final int from,
+    		final int to,
+    		final int minInclusive,
+    		final int maxInclusive) {
+    	
+    	final int len = string.length();
+    	if(len > from) {
+    		final String part = string.substring(from, Math.min(to, len));
+//    		System.out.println("part=[" + part + "]");
+    		if(isIntNumber(part) == false) {
+    			return false;
+    		}
+    		final int number = Integer.parseInt(part);
+    		return number <= maxInclusive && number >= minInclusive;
+    	}
+    	return true; // string is not long enough to check anything
+    }
+
+    private static boolean validateTimestampChar(
+    		final String string,
+    		final int pos,
+    		final char c
+    	) {
+    	if(string.length() > pos && string.charAt(pos) != c) {
+//			System.err.println("charAt(" + pos + ") [" + string.charAt(pos) + "] != [" + c + "]");
+			return false;
+    	}
+    	return true;
+    }
+    		
+    /**
+     * @param string something like "yyyy-MM-dd HH:mm:ss.SSSSSS"
+     */
+    public static boolean validateTimestampPart(final String string) {
+//    	System.out.println("validating part [" + string + "]");
+    	final int len = string.length();
+    	
+//    	System.out.println("len: " + len);
+    	if(len > TIMESTAMP_FORMAT.length()) {
+//    		System.err.println("overlength!");
+    		return false;
+    	}
+
+    	// msec
+    	if(validateTimestampNumber(string, 20, 26, 0, 999999) == false) {
+    		return false;
+    	}
+
+    	if(validateTimestampChar(string, 19, '.') == false) {
+    		return false;
+    	}
+
+    	// sec
+    	if(validateTimestampNumber(string, 17, 19, 0, 59) == false) {
+    		return false;
+    	}
+
+    	if(validateTimestampChar(string, 16, ':') == false) {
+    		return false;
+    	}
+    	
+    	// minute
+    	if(validateTimestampNumber(string, 14, 16, 0, 59) == false) {
+    		return false;
+    	}
+
+    	if(validateTimestampChar(string, 13, ':') == false) {
+    		return false;
+    	}
+    	
+    	// hour
+    	if(validateTimestampNumber(string, 11, 13, 0, 23) == false) {
+    		return false;
+    	}
+
+    	if(validateTimestampChar(string, 10, ' ') == false) {
+    		return false;
+    	}
+
+    	// day
+    	if(validateTimestampNumber(string, 8, 10, 1, 31) == false) {
+    		return false;
+    	}
+
+    	if(validateTimestampChar(string, 7, '-') == false) {
+    		return false;
+    	}
+
+    	// month
+    	if(validateTimestampNumber(string, 5, 7, 1, 12) == false) {
+    		return false;
+    	}
+
+    	if(validateTimestampChar(string, 4, '-') == false) {
+    		return false;
+    	}
+    	
+    	// year
+    	if(validateTimestampNumber(string, 0, 4, 0, 9999) == false) {
+    		return false;
+    	}
+
+    	return true;
+    }
+    
+    private static boolean isIntNumber(final String string) {
+    	for (int i = 0; i < string.length(); i++) {
+			final char c = string.charAt(i);
+			if(Character.isDigit(c) == false) {
+				return false;
+			}
+		}
+    	return true;
+    }
+    
+    public static String formatTimestamp(final long timestamp) {
+    	return TIMESTAMP_FORMATTER.format(new Date(timestamp));
+    }
+    
+    
     
     public static void main(final String[] args) throws ParseException {
         final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
